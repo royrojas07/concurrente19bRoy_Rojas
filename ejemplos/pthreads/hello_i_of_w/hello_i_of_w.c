9e6de9ad55710763f1d0a537878a94a0d48501c0
Roy Andres Rojas Angulo <b76629@ecci.ucr.ac.cr@Lab-102-08.ecci.ucr.ac.cr>
2019-09-10 09:40:10 -0600

    actualizacion a hello_i_of_w.c

--- ejemplos/pthreads/hello_i_of_w/hello_i_of_w.c
+++ ejemplos/pthreads/hello_i_of_w/hello_i_of_w.c
@@ -12,34 +12,48 @@ typedef struct {
 	shared_data_t * shared_data;
 } private_data_t;
 
-void * run( void * data ){
-	private_data_t * private_data = (private_data_t*)data;
-	shared_data_t * shared_data = private_data->shared_data;
-	printf( "HELLO WORLD from secondary thread %zu of %zu\n", private_data->thread_num, shared_data->t_count );
-	return NULL;
-}
+int create_threads( shared_data_t * shared_data );
+void * run( void * data );
 
 int main( int argc, char* argv[] ){
-	size_t t_count = sysconf(_SC_NPROCESSORS_ONLN);
-	if( argc >= 2 )
-		t_count = strtoull( argv[1], NULL, 10 );
-
-	pthread_t * threads = (pthread_t*) malloc( t_count * sizeof(pthread_t) );
-	
-	if( !threads )
-		return fprintf( stderr, "error: could not allocate memory for %zu threads\n", t_count ), 1;
-	
 	shared_data_t * shared_data = (shared_data_t*) calloc( 1, sizeof(shared_data_t) );
 	if( !shared_data )
-		return fprintf( stderr, "error: could not allocate shared memory for %zu threads\n", t_count ), 2;
+		return fprintf( stderr, "error: could not allocate shared memory for %zu threads\n", shared_data->t_count ), 2;
+
+	shared_data->t_count = sysconf(_SC_NPROCESSORS_ONLN);
+	if( argc >= 2 )
+		shared_data->t_count = strtoull( argv[1], NULL, 10 );
+
+	struct timespec start_time;
+	clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+	int error = create_threads(shared_data);
+	if( error ){
+		return error;
+	}
 	
-	shared_data->t_count = t_count;
+	struct timespec finish_time;
+	clock_gettime(CLOCK_MONOTONIC, &finish_time);
+
+	double elapsed_seconds = finish_time.tv_sec - start_time.tv_sec
+		+ 1e-9 * (finish_time.tv_nsec - start_time.tv_nsec);
+
+	printf("Hello execution time %.9lfs\n", elapsed_seconds);
 	
-	private_data_t * private_data = (private_data_t*) calloc( t_count, sizeof(private_data_t) );
+	free( shared_data );
+	return 0;
+}
+
+int create_threads( shared_data_t * shared_data ){
+	pthread_t * threads = (pthread_t*) malloc( shared_data->t_count * sizeof(pthread_t) );
+	if( !threads )
+		return fprintf( stderr, "error: could not allocate memory\n" ), 1;
+	
+	private_data_t * private_data = (private_data_t*) calloc( shared_data->t_count, sizeof(private_data_t) );
 	if( !private_data )
-		return fprintf( stderr, "error: could not allocate private memory for %zu threads\n", t_count ), 3;
+		return fprintf( stderr, "error: could not allocate private memory for %zu threads\n", shared_data->t_count ), 3;
 	
-	for( size_t index = 0; index < t_count; ++index ){
+	for( size_t index = 0; index < shared_data->t_count; ++index ){
 		private_data[index].thread_num = index;
 		private_data[index].shared_data = shared_data;
 		pthread_create( &threads[index], NULL, run, &private_data[index] );
@@ -47,11 +61,17 @@ int main( int argc, char* argv[] ){
 	
 	printf( "HELLO WORLD from main thread\n" );
 
-	for( size_t index = 0; index < t_count; ++index )
+	for( size_t index = 0; index < shared_data->t_count; ++index )
 		pthread_join( threads[index], NULL );
 	
 	free( private_data );
-	free( shared_data );
 	free( threads );
 	return 0;
+}
+
+void * run( void * data ){
+	private_data_t * private_data = (private_data_t*)data;
+	shared_data_t * shared_data = private_data->shared_data;
+	printf( "HELLO WORLD from secondary thread %zu of %zu\n", private_data->thread_num, shared_data->t_count );
+	return NULL;
 }
