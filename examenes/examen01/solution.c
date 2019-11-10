61d609a366cfbc79e7103aee632f406cd948166a
royrojas07 <royrojasa@gmail.com>
2019-11-10 00:26:02 -0600

    exam_commit_02

--- examenes/examen01/solution.c
+++ examenes/examen01/solution.c
@@ -2,65 +2,88 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 typedef struct {
 	int thread_count;
-	char ** sudoku;
+	int ** sudoku;
 	int block_size;
-	char ** valid_char;
-	int ** char_errors;
-	int c_err_cnt;
-	pthread_mutex_t * mutex;
+	//char ** valid_char;
+	//int ** char_errors;
+	//int c_err_cnt;
+	//pthread_mutex_t * mutex;
 	sem_t * semaphores;
 } shared_data_t;
 
 typedef struct {
 	int thread_num;
 	shared_data_t * shared_data;
-	int err_count;
-	int ** errors;
-	int * char_count;
+	//int err_count;
+	int error_count;
+	//int ** errors;
+	int * errors;
+	//int * char_count;
+	int * num_appearances;
 } private_data_t;
 
-int main( int argc, char * argv[] ){
+void clean( int * array, int array_size );
+void print_errors( char error_type, int * coordenates, int size );
+int ** create_table( int size );
+void destroy_table( int ** table, int size );
+int load_sudoku( shared_data_t * shared_data );
+int create_threads( shared_data_t * shared_data );
+void * validate_sudoku( void * data );
+
+int main(){
 	shared_data_t * shared_data = (shared_data_t *) calloc( 1, sizeof(shared_data_t) );
 	
 	if( scanf( "%d", &shared_data->block_size ) != 1 )
 		return fprintf( stderr, "No se indica tamaño de bloque" ), 1;
 	
-	int error = load_sudoku( argv, shared_data );
+	shared_data->thread_count = sysconf( _SC_NPROCESSORS_ONLN );
+	
+	int error = load_sudoku( shared_data );
 	if( !error ){
-		for( int i = 0; i < shared_data->block_size * shared_data->block_size; ++i )
-			shared_data->valid_char[i] = itoa(i);
-		
-		pthread_mutex_init( shared_data->mutex, NULL );
-		sem_init( semaphores[0], 0, 1 );
-		for( int i = 1; i < thread_count; ++i )
-			sem_init( semaphores[i], 0, 0 );
+		shared_data->semaphores = (sem_t *) malloc( shared_data->thread_count * sizeof(sem_t) );
+		sem_init( &shared_data->semaphores[0], 0, 1 );
+		for( int i = 1; i < shared_data->thread_count; ++i )
+			sem_init( &shared_data->semaphores[i], 0, 0 );
 		
 		create_threads( shared_data );
+		
+		for( int i = 0; i < shared_data->thread_count; ++i )
+			sem_destroy( &shared_data->semaphores[i] );
+		free( shared_data->semaphores );
 	}
+	
+	free( shared_data );
+	return 0;
 }
 
-int load_sudoku( char * argv[], shared_data_t * shared_data ){
+int load_sudoku( shared_data_t * shared_data ){
 	if( shared_data->block_size < 0 )
 		return fprintf( stderr, "Tamaño bloque inválido" ), 1;
 	
-	shared_data->sudoku = create_table( shared_data->block_size );
-	if( !shared_data->sudoku )
-		fprintf( stderr, "No se pudo alojar memoria para un tablero %d^2", &shared_data->block_size ), 2;
-	
 	int table_size = shared_data->block_size * shared_data->block_size;
-	for( int i = 1; i <= table_size; ++i ){
-		for( int j = 1; j <= table_size; ++j ){
-			scanf( "%c", &shared_data->sudoku[i-1][j-1] );
+	shared_data->sudoku = create_table( table_size );
+	if( !shared_data->sudoku )
+		return fprintf( stderr, "No se pudo alojar memoria para un tablero %d^2", shared_data->block_size ), 2;
+	
+	for( int i = 0; i < table_size; ++i ){
+		for( int j = 0; j < table_size; ++j ){
+			if( !scanf( "%d", &shared_data->sudoku[i][j] ) ){
+				char c = getchar();
+				if( c != '.' )
+					shared_data->sudoku[i][j] = -1;
+			}
+			printf( "%d", shared_data->sudoku[i][j] );
 		}
+		printf( "\n" );
 	}
 	return 0;
 }
 
 int create_threads( shared_data_t * shared_data ){
-	shared_data->thread_count = sysconf( _SC_NPROCESSORS_ONLN );
 	pthread_t * threads = (pthread_t *) malloc( shared_data->thread_count * sizeof(pthread_t) );
 	if( threads == NULL )
 		return fprintf( stderr, "No se pudieron crear tal cantidad de threads" ), 1;
@@ -70,14 +93,23 @@ int create_threads( shared_data_t * shared_data ){
 		return fprintf( stderr, "No se pudo alojar memoria privada para tal cantidad de threads" ),
 			free( threads ), 2;
 	
+	int table_size = shared_data->block_size * shared_data->block_size;
+	
 	for( int i = 0; i < shared_data->thread_count; ++i ){
 		private_data[i].thread_num = i;
 		private_data[i].shared_data = shared_data;
+		private_data[i].num_appearances = calloc( table_size, sizeof(int) );
+		private_data[i].errors = malloc( table_size * 2 * sizeof(int) );
 		pthread_create( &threads[i], NULL, validate_sudoku, &private_data[i] );
 	}
 	
 	for( int i = 0; i < shared_data->thread_count; ++i )
 		pthread_join( threads[i], NULL );
+	
+	for( int i = 0; i < shared_data->thread_count; ++i ){
+		free( private_data[i].num_appearances );
+		free( private_data[i].errors );
+	}
 	
 	free( private_data );
 	free( threads );
@@ -89,42 +121,139 @@ void * validate_sudoku( void * data ){
 	shared_data_t * shared_data = private_data->shared_data;
 	
 	int table_size = shared_data->block_size * shared_data->block_size;
+	int thread_count = shared_data->thread_count;
+	int thread_num = private_data->thread_num;
+	int block_size = shared_data->block_size;
+	int number;
+	
 	/* for con mapeo cíclico para filas */
-	for( int i = private_data->thread_num; i < table_size; i += shared_data->thread_count ){
+	for( int i = thread_num; i < table_size; i += thread_count ){
 		for( int j = 0; j < table_size; ++j ){
-			if( exists_in( shared_data->valid_char, shared_data->sudoku[i][j] ) ){
-				int num = atoi( shared_data->sudoku[i][j] );
-				if( private_data->char_count[num] != 0 ){
-					private_data->errors[private_data->err_count][0] = 0;
-					private_data->errors[private_data->err_count][1] = i;
-					private_data->errors[private_data->err_count][2] = j;
+			number = shared_data->sudoku[i][j];
+			//if( exists_in( shared_data->valid_char, shared_data->sudoku[i][j] ) ){
+			if( number > 0 && number <= table_size ){
+				//int num = atoi( shared_data->sudoku[i][j] );
+				//if( private_data->char_count[num] != 0 ){
+				if( private_data->num_appearances[number-1] != 0 ){
+					//private_data->errors[private_data->error_count][0] = 0;
+					//private_data->errors[private_data->error_count][1] = i;
+					//private_data->errors[private_data->error_count][2] = j;
+					private_data->errors[private_data->error_count++] = i+1;
+					private_data->errors[private_data->error_count++] = j+1;
 				}
-				private_data->char_cnt[num]++;
-			} else {
-				pthread_mutex_lock( shared_data->mutex );
-				shared_data->char_errors[shared_data->c_err_cnt++][0] = i;
-				shared_data->char_errors[shared_data->c_err_cnt][1] = j;
-				pthread_mutex_unlock( shared_data->mutex );
+				private_data->num_appearances[number-1]++;
+			}
+		}
+		clean( private_data->num_appearances, table_size );
+	}
+	sem_wait( &shared_data->semaphores[private_data->thread_num] );
+	print_errors( 'r', private_data->errors, private_data->error_count );
+	if( private_data->thread_num == shared_data->thread_count-1 )
+		sem_post( &shared_data->semaphores[0] );
+	else
+		sem_post( &shared_data->semaphores[private_data->thread_num+1] );
+	private_data->error_count = 0;
+	clean( private_data->num_appearances, table_size );
+	//clean_matrix( errors, table_size );
+	
+	/* for con mapeo cíclico para columnas */
+	for( int i = 0; i < table_size; ++i ){
+		for( int j = thread_num; j < table_size; j += thread_count ){
+			number = shared_data->sudoku[i][j];
+			if( number > 0 && number <= table_size ){
+				if( private_data->num_appearances[number-1] != 0 ){
+					private_data->errors[private_data->error_count++] = i+1;
+					private_data->errors[private_data->error_count++] = j+1;
+				}
+				private_data->num_appearances[number-1]++;
+			}
+		}
+		clean( private_data->num_appearances, table_size );
+	}
+	
+	sem_wait( &shared_data->semaphores[private_data->thread_num] );
+	print_errors( 'c', private_data->errors, private_data->error_count );
+	if( private_data->thread_num == shared_data->thread_count-1 )
+		sem_post( &shared_data->semaphores[0] );
+	else
+		sem_post( &shared_data->semaphores[private_data->thread_num+1] );
+	private_data->error_count = 0;
+	clean( private_data->num_appearances, table_size );
+	
+	int initial_row;
+	int initial_column;
+	/* for con mapeo cíclico para bloques */
+	for( int k = thread_num; k < table_size; k += thread_count ){
+		initial_row = k - k % block_size;
+		for( int i = initial_row; i < initial_row + block_size; ++i ){
+			initial_column = (k % block_size)*block_size;
+			for( int j = initial_column; j < initial_column + block_size; ++j ){
+				number = shared_data->sudoku[i][j];
+				if( number > 0 && number <= table_size ){
+					if( private_data->num_appearances[number-1] != 0 ){
+						private_data->errors[private_data->error_count++] = i+1;
+						private_data->errors[private_data->error_count++] = j+1;
+					}
+					private_data->num_appearances[number-1]++;
+				}
+			}
+		}
+		clean( private_data->num_appearances, table_size );
+	}
+	
+	sem_wait( &shared_data->semaphores[private_data->thread_num] );
+	print_errors( 'b', private_data->errors, private_data->error_count );
+	if( private_data->thread_num == shared_data->thread_count-1 )
+		sem_post( &shared_data->semaphores[0] );
+	else
+		sem_post( &shared_data->semaphores[private_data->thread_num+1] );
+	private_data->error_count = 0;
+	clean( private_data->num_appearances, table_size );
+	
+	/* mapeo cíclico para encontrar errores de caracteres */
+	for( int i = thread_num; i < table_size; i += thread_count ){
+		for( int j = 0; j < table_size; ++j ){
+			if( shared_data->sudoku[i][j] == -1 ){
+				private_data->errors[private_data->error_count++] = i+1;
+				private_data->errors[private_data->error_count++] = j+1;
 			}
 		}
 	}
 	
 	sem_wait( &shared_data->semaphores[private_data->thread_num] );
-	print_errors( private_data->errors );
-	sem_post( &shared_data->semaphores[private_data->thread_num+1] );
-	private_data->err_count = 0;
-	clean_matrix( errors, table_size );
+	print_errors( 'e', private_data->errors, private_data->error_count );
+	if( private_data->thread_num != shared_data->thread_count-1 )
+		sem_post( &shared_data->semaphores[private_data->thread_num+1] );
 	
-	/* for con mapeo cíclico para columnas */
-	for( int i = 0; i < table_size; ++i ){
-		for( int j = private_data->thread_num; j < table_size; j += shared_data->thread_count ){
-			/* comentarios escritos en el examen */
-		}
+	return NULL;
+}
+
+void clean( int * array, int array_size ){
+	for( int i = 0; i < array_size; ++i )
+		array[i] = 0;
+}
+
+void print_errors( char error_type, int * coordenates, int size ){
+	for( int i = 0; i < size; i += 2 )
+		printf( "%c%d, %d\n", error_type, coordenates[i], coordenates[i+1] );
+}
+
+int ** create_table( int size ){
+	int ** table = malloc( size * sizeof(int *) );
+	if( !table )
+		return NULL;
+	
+	for( int i = 0; i < size; ++i ){
+		table[i] = calloc( size, sizeof(int) );
+		if( !table[i] )
+			return destroy_table( table, size ), NULL;
 	}
 	
-	sem_wait( &shared_data->semaphores[private_data->thread_num] );
-	print_errors( private_data->errors );
-	sem_post( &shared_data->semaphores[private_data->thread_num+1] );
-	private_data->err_count = 0;
-	clean_matrix( errors, table_size );
+	return table;
+}
+
+void destroy_table( int ** table, int size ){
+	for( int i = 0; i < size; ++i )
+		free( table[i] );
+	free( table );
 }
