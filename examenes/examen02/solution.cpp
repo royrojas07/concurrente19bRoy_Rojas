503d66ac34cd0a43aecfe93a5aad68ee223bcdf3
royrojas07 <royrojasa@gmail.com>
2019-12-17 03:45:28 -0600

    exam solution last change

--- examenes/examen02/solution.cpp
+++ examenes/examen02/solution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <stdlib>
+#include <cstdlib>
+#include <cstdio>
 #include <mpi.h>
+#include <vector>
 
 typedef struct{
 	int rows;
@@ -20,8 +22,8 @@ void search_clouds( data_t * data );
 void expand_cloud( int r, int c, data_t * data );
 bool exists( int r, int c, data_t * data );
 void print_clouds( std::vector<int> cloud_areas );
-double ** read_image( FILE * file );
-int ** get_mirror( int begin, int end );
+double ** read_image( data_t * data );
+int ** get_mirror( int begin, int end, int cols );
 int my_start( int rank, int D, int W );
 void update_last_row( int to_mod, int mod, data_t * data );
 void destroy_image( double ** image, int size );
@@ -30,11 +32,11 @@ void destroy_mirror( int ** mirror, int size );
 int main( int argc, char * argv[] ){
 	MPI_Init( &argc, &argv );
 
-	data_t data = ( data_t * ) calloc( 1, sizeof( data_t ) );
+	data_t * data = ( data_t * ) calloc( 1, sizeof( data_t ) );
 	data->file = fopen( argv[1], "r" );
 	fscanf( data->file, "%d", &data->rows );
 	fscanf( data->file, "%d", &data->cols );
-	data->image = read_image( data->file );
+	data->image = read_image( data );
 	data->min = atoi( argv[2] );
 	data->max = atoi( argv[3] );
 
@@ -45,7 +47,7 @@ int main( int argc, char * argv[] ){
 
 	data->init_row = my_start( rank, data->rows, proc_cnt );
 	data->fin_row = my_start( rank+1, data->rows, proc_cnt );
-	data->mirror = get_mirror( data->init_row, data->fin_row );
+	data->mirror = get_mirror( data->init_row, data->fin_row, data->cols );
 
 	search_clouds( data );
 
@@ -53,7 +55,7 @@ int main( int argc, char * argv[] ){
 	int next = rank+1 == proc_cnt ? 0 : rank+1;
 	int prev_areas_count = 0;
 	std::vector<int> prev_areas;
-	int * prev_last_row = malloc( data->cols * sizeof( int ) );
+	int * prev_last_row = ( int * ) malloc( data->cols * sizeof( int ) );
 	int range = data->fin_row - data->init_row;
 
 	if( rank != 0 ){
@@ -84,7 +86,7 @@ int main( int argc, char * argv[] ){
 		else
 			print_clouds( prev_areas );
 	} else {
-		int areas_count = cloud_areas.size();
+		int areas_count = data->cloud_areas.size();
 		MPI_Send( &areas_count, 1, MPI::INT, next, 0, MPI_COMM_WORLD );
 		MPI_Send( &data->cloud_areas[0], areas_count, MPI::INT, next, 0, MPI_COMM_WORLD );
 		MPI_Send( &data->mirror[range-1][0], data->cols, MPI::INT, next, 0, MPI_COMM_WORLD );
@@ -97,8 +99,8 @@ int main( int argc, char * argv[] ){
 }
 
 void search_clouds( data_t * data ){
-	for( int r = data->init_row; row < data->fin_row; ++r ){
-		for( int c = 0; col < data->cols; ++c ){
+	for( int r = data->init_row; r < data->fin_row; ++r ){
+		for( int c = 0; c < data->cols; ++c ){
 			if( data->mirror[r][c] == 0 ){
 				if( data->min <= data->image[r][c] && data->max >= data->image[r][c] ){
 					++data->cloud_count;
@@ -113,7 +115,7 @@ void search_clouds( data_t * data ){
 }
 
 void expand_cloud( int r, int c, data_t * data ){
-	if( exists( r, c, data->init_row, data->fin_row ) && data->mirror[r][c] == 0
+	if( exists( r, c, data ) && data->mirror[r][c] == 0 &&
 		data->min <= data->image[r][c] && data->max >= data->image[r][c] ){
 			data->mirror[r][c] = data->cloud_count;
 			++data->cloud_areas[data->cloud_count-1];
@@ -136,9 +138,9 @@ void print_clouds( std::vector<int> cloud_areas ){
 }
 
 double ** read_image( data_t * data ){
-	double ** image = malloc( data->rows * sizeof( double * ) );
+	double ** image = ( double ** ) malloc( data->rows * sizeof( double * ) );
 	for( int i = 0; i < data->rows; ++i )
-		image[i] = malloc( data->cols * sizeof( double ) );
+		image[i] = ( double * ) malloc( data->cols * sizeof( double ) );
 
 	for( int i = 0; i < data->rows; ++i ){
 		for( int j = 0; i < data->cols; ++i )
@@ -148,10 +150,12 @@ double ** read_image( data_t * data ){
 	return image;
 }
 
-int get_mirror( int begin, int end ){
-	int ** mirror = malloc( (end - begin) * sizeof( double * ) );
-	for( int i = 0; i < end; ++i )
-		image[i] = calloc( data->cols, sizeof( double ) );
+int ** get_mirror( int begin, int end, int cols ){
+	int ** mirror = ( int ** ) malloc( (end - begin) * sizeof( int * ) );
+	for( int i = 0; i < end-begin; ++i )
+		mirror[i] = ( int * ) calloc( cols, sizeof( int ) );
+
+	return mirror;
 }
 
 int my_start( int rank, int D, int W ){
